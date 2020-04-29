2f156ff2f66f02aa671cdb85d1fb5d400bf55e39
Jonathan Guiang <jkguiang@gmail.com>
2020-04-28 20:45:44 -0700

    added custom year

--- NanoCORE/Nano.h
+++ NanoCORE/Nano.h
@@ -4886,7 +4886,7 @@ protected:
     bool loaded_run_;
     Int_t year_;
 public:
-    void Init(TTree *tree);
+    void Init(TTree *tree, int year);
     void setYear(TTree* tree);
     void PrintUsage();
     void GetEntry(unsigned int idx);
