f35f5595b3aa2fe333ca7fc03f1c2e1231c4cd7d
Jonathan Guiang <jkguiang@gmail.com>
2020-07-13 17:46:45 -0700

    added batch-friendly doAll

---
+++ studies/ttbarCR/doAll.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+from argparse import ArgumentParser, RawTextHelpFormatter
+import ROOT
+ROOT.SetMemoryPolicy(ROOT.kMemoryStrict)
+import glob
+
+def doAll(input_file, is_data):
+    # Load .so files
+    ROOT.gROOT.ProcessLine(".L ./NanoCORE/NANO_CORE.so");
+    ROOT.gROOT.ProcessLine(".L ./control.C+");
+    ROOT.gROOT.ProcessLine(".L ScanChain.C+");
+    # Parse samples
+    # Make TChain
+    tchain = ROOT.TChain("Events")
+    tchain.Add(input_file)
+    # Run ScanChain
+    ROOT.ScanChain(tchain, "output.root", is_data)
+
+    return
+
+if __name__ == "__main__":
+    # CLI
+    argparser = ArgumentParser(
+        description='Run looper',
+        formatter_class=RawTextHelpFormatter
+    )
+    # Input file path
+    argparser.add_argument(
+        "--input_file", 
+        type=str, 
+        default="",
+        help="Input file path"
+    )
+    # Dilepton
+    argparser.add_argument(
+        "--is_data", 
+        action="store_true",
+        default=True,
+        help="Input is not simulation"
+    )
+    # Get args
+    args = argparser.parse_args()
+
+    doAll(args.input_file, args.is_data)
