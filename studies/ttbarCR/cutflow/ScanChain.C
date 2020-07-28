508a21a83c0a1700e19cec416af03e5917a1fe5e
aaarora <aaarora@ucsd.edu>
2020-07-28 11:04:51 -0700

    added plots nb, updated cutflow

--- studies/ttbarCR/cutflow/ScanChain.C
+++ studies/ttbarCR/cutflow/ScanChain.C
@@ -8,14 +8,14 @@
 
 #include "./NanoCORE/Nano.h"
 #include "./NanoCORE/SSSelections.cc"
-
+#include "./scalefactors.C"
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
 using namespace tas;
 
-void analysis(int &gen_lept_event, int &gen_elec_event_t, int &gen_mu_event_t, int &gen_one_and_one_t, int &gen_opp_ch_t, int &gen_two_b_t, int &lept_event, int &elec_event_t, int &mu_event_t, int &one_and_one_t, int &opp_ch_t, int &pass_hlt_t, int &two_btag_t, bool isData){
+void analysis(int &gen_lept_event, int &gen_elec_event_t, int &gen_mu_event_t, int &gen_one_and_one_t, int &gen_opp_ch_t, int &gen_two_b_t, int &lept_event, int &elec_event_t, int &mu_event_t, int &one_and_one_t, int &opp_ch_t, int &pass_hlt_t, int &two_btag_t, float &elecID_w, float &muID_w, float &muIso_w, bool isData){
 	// Iter over GenParticles
 	int numGenMu = 0;
 	int numGenElec = 0;
@@ -23,20 +23,19 @@ void analysis(int &gen_lept_event, int &gen_elec_event_t, int &gen_mu_event_t, i
 	int gen_el_id = 0;
 	int gen_mu_id = 0;
 	for (unsigned int i = 0; i < nGenPart(); i++) {
-		int m_index = fabs(GenPart_genPartIdxMother().at(i));
-		//if(fabs(GenPart_pdgId().at(m_index)) == 24 && (GenPart_statusFlags().at(i) & (1<<13)) == (1<<13)){
-		if(fabs(GenPart_pdgId().at(m_index)) == 24){
-			if(fabs(GenPart_pdgId().at(i)) == 13) {
+		int m_index = abs(GenPart_genPartIdxMother().at(i));
+		if(abs(GenPart_pdgId().at(m_index)) == 24){
+			if(abs(GenPart_pdgId().at(i)) == 13 && GenPart_pt().at(i) > 25) {
 				numGenMu++;
 				gen_mu_id = i;
 			}
-			else if(fabs(GenPart_pdgId().at(i)) == 11) {
+			else if(abs(GenPart_pdgId().at(i)) == 11 && GenPart_pt().at(i) > 25) {
 				numGenElec++;
 				gen_el_id = i;
 			}	
 		}
-		if(fabs(GenPart_pdgId().at(m_index)) == 6){
-			if(fabs(GenPart_pdgId().at(i)) == 5){
+		else if(abs(GenPart_pdgId().at(m_index)) == 6){
+			if(abs(GenPart_pdgId().at(i)) == 5 && fabs(GenPart_eta().at(i)) < 2.5 && GenPart_pt().at(i) > 30){
 				numGenb++;
 			}
 		}
@@ -58,7 +57,9 @@ void analysis(int &gen_lept_event, int &gen_elec_event_t, int &gen_mu_event_t, i
 
 	if (gen_one_and_one && (GenPart_pdgId().at(gen_mu_id) * GenPart_pdgId().at(gen_el_id) < 0)) {gen_opp_ch = true;}
 	
+	//cout << GenPart_pdgId().at(gen_mu_id) * GenPart_pdgId().at(gen_el_id) << endl;	
 	if (gen_opp_ch && numGenb == 2) {gen_two_b = true;}	
+	//if (numGenb == 2){gen_two_b = true;}
 	// Iter over Reco Leptons	
 	Leptons leptons = getLeptons();
 	int numElec = 0;
@@ -92,7 +93,6 @@ void analysis(int &gen_lept_event, int &gen_elec_event_t, int &gen_mu_event_t, i
 	bool pass_hlt = false;
 	bool two_btag = false;
 	// Conditionals
-	//if (numElec == 0 && numMu == 0) {return;}
 	
 	if (numElec > 0 || numMu > 0) {lept_event++;}
 
@@ -104,7 +104,7 @@ void analysis(int &gen_lept_event, int &gen_elec_event_t, int &gen_mu_event_t, i
 	
 	if (one_and_one && (elec.charge() == -mu.charge())) {opp_ch = true;}
 	
-	if(opp_ch && (HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL() || HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL() || HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ() || HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ())) { pass_hlt = true; }
+	if (opp_ch && (HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL() || HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL() || HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ() || HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ())) {pass_hlt = true;}
 
 	// 2016 legacy wp	
 	float tight_working_point = 0.7221;	
@@ -115,18 +115,19 @@ void analysis(int &gen_lept_event, int &gen_elec_event_t, int &gen_mu_event_t, i
 	int num_tagged_b_tight = 0;	
 	// Iter Over Jets
 	
-	if (numElec != 0 && numMu != 0){
-		for (unsigned int i = 0; i < nJet(); i++) {
-			if(fabs(Jet_eta().at(i)) > 2.4) continue;
-			if(Jet_pt().at(i) < 30) continue;
+	for (unsigned int i = 0; i < nJet(); i++) {
+		if(fabs(Jet_eta().at(i)) > 2.4) continue;
+		if(Jet_pt().at(i) < 30) continue;
+		if (numElec != 0 && numMu != 0){
 			if(Jet_jetId().at(i) == Electron_jetIdx().at(elec.idx()) || Jet_jetId().at(i) == Muon_jetIdx().at(mu.idx())) continue;
-			if(Jet_btagDeepFlavB().at(i) >= tight_working_point) {
-				num_tagged_b_tight++;
-			}
+		}
+		if(Jet_btagDeepFlavB().at(i) >= tight_working_point) {
+			num_tagged_b_tight++;
 		}
 	}
+
 	if (pass_hlt && (num_tagged_b_tight == 2)) {two_btag = true;}
-	
+	//if (num_tagged_b_tight == 2) {two_btag = true;}
 	// Update Global Count
 	if (gen_elec_event) {gen_elec_event_t++;}
 	if (gen_mu_event) {gen_mu_event_t++;}
@@ -139,6 +140,17 @@ void analysis(int &gen_lept_event, int &gen_elec_event_t, int &gen_mu_event_t, i
 	if (opp_ch) {opp_ch_t++;}
 	if (pass_hlt) {pass_hlt_t++;}
 	if (two_btag) {two_btag_t++;}
+
+	float xsec_w = 831.76 * 16357.0 / 6492749;
+	float el_w = elecIDScaleFactors(elec.pt(), elec.eta());
+	float mu_w = muonMediumIDScaleFactors(mu.pt(), mu.eta());
+	float mu_is_w = muonLooseIsoMediumIDScaleFactors(mu.pt(), mu.eta());
+	if (two_btag) {
+		elecID_w += xsec_w * el_w;
+		muID_w += xsec_w * el_w * mu_w;
+		muIso_w += xsec_w * el_w * mu_w * mu_is_w;
+	}
+	
 }        
 
 
@@ -169,7 +181,10 @@ int ScanChain(TChain *ch, bool isData) {
 	int opp_ch_t = 0;
 	int pass_hlt_t = 0;
 	int two_btag_t = 0;
-	
+	float elecID_w = 0;
+	float muID_w = 0;
+	float muIso_w = 0;
+
     // File loop
     while ( (currentFile = (TFile*)fileIter.Next()) ) {
         // Open file
@@ -189,7 +204,7 @@ int ScanChain(TChain *ch, bool isData) {
             // Update progress
             nEventsTotal++;
             /* Analysis code */
-			analysis(gen_lept_event, gen_elec_event_t, gen_mu_event_t, gen_one_and_one_t, gen_opp_ch_t, gen_two_b_t, lept_event, elec_event_t, mu_event_t, one_and_one_t, opp_ch_t, pass_hlt_t, two_btag_t, isData);		
+			analysis(gen_lept_event, gen_elec_event_t, gen_mu_event_t, gen_one_and_one_t, gen_opp_ch_t, gen_two_b_t, lept_event, elec_event_t, mu_event_t, one_and_one_t, opp_ch_t, pass_hlt_t, two_btag_t, elecID_w, muID_w, muIso_w, isData);		
 			} // END event loop
         // Clean up
         delete file;
@@ -209,6 +224,9 @@ int ScanChain(TChain *ch, bool isData) {
 	cout << "Num Opposite Sign Events: " << opp_ch_t << endl;
 	cout << "Num Passed HLTs Events: " << pass_hlt_t << endl;
 	cout << "Num 2 bTag Events: " << two_btag_t << endl;
+	cout << "Num elecID_w: " << elecID_w << endl;
+	cout << "Num muID_w: " << muID_w << endl;
+	cout << "Num muIso_w: " << muIso_w << endl;
  
     return 0;
 }
