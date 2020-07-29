dbe32027a50e685a2774470f2aca9c6467ad6d22
Jonathan Guiang <jkguiang@gmail.com>
2020-07-29 12:56:50 -0700

    updated for new looper

--- studies/ttbarCR/batch/createTarball.sh
+++ studies/ttbarCR/batch/createTarball.sh
@@ -7,5 +7,6 @@ fi
 tar -hcJf ./inputs/package.tar.xz \
     ../NanoCORE/*.h ../NanoCORE/*.cc \
     ../NanoCORE/NANO_CORE.so \
-    ../ScanChain.C ../control.h ../control.C \
+    ../ScanChain.C ../control.h ../control.C ../HLTs.C\
+    ../sample_info.json \
     ../doAll.py \
